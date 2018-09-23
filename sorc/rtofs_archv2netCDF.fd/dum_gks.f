dcce6b64dfb64aa7dd78d6632440559d3f510810
Shastri.Paturi@noaa.gov <Shastri.Paturi@t21a1.ncep.noaa.gov>
2018-09-23 17:02:05 +0000

    Cleaned RB-1.2.0 branch

--- sorc/rtofs_archv2netCDF.fd/dum_gks.f
+++
@@ -1,6 +0,0 @@
-      subroutine clsgks
-c
-c --- dummy version for no graphics cases.
-c
-      return
-      end
--- sorc/rtofs_atmforcing.fd/mod_flags.f90
+++
@@ -1,4 +0,0 @@
-MODULE mod_flags
-PUBLIC
-INTEGER, PARAMETER:: mapflg_mer=0,mapflg_ort=5,mapflg_tripol=12 
-END MODULE mod_flags
