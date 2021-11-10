f74dca7ea4b7c4a05888f6c10ee7a434c69415a7
Dan Iredell <Dan.Iredell@clogin02.cactus.wcoss2.ncep.noaa.gov>
2021-11-10 17:36:07 +0000

    change libs/build_libs.sh to use versions

--- libs/build_libs.sh
+++ libs/build_libs.sh
@@ -1,9 +1,28 @@
 #!/bin/sh 
-set -x 
+
+#module purge
+#odule use ../modulefiles
+#odule load build_rtofs_libs_wcoss2.module
+#odule list
+#xit
+
+set -x
+
+BASE=`pwd`
+dir_mod0="$(dirname ${BASE})"
+echo ${dir_mod0}
+
+source ${dir_mod0}/versions/build.ver
 
 module purge
-module use ../modulefiles
-module load build_rtofs_libs_wcoss2.module
+module load envvar/${envvar_ver}
+module load intel/${intel_ver}
+module load PrgEnv-intel/${PrgEnv_intel_ver}
+module load craype/${craype_ver}
+module load cray-mpich/${cray_mpich_ver}
+module load bacio/${bacio_ver}
+module load w3nco/${w3nco_ver}
+module load g2/${g2_ver}
 module list
 
  mkdir -p incmod
