4e070d2952d1eaf994b64ea5db02b1769719b4ec
sanAkel <santha.akella@gmail.com>
2025-12-02 00:11:58 +0000

    add more to the module, change makefile

--- sorc/rtofs_da.fd/observations/read_bin.F90
+++ sorc/rtofs_da.fd/observations/read_bin.F90
@@ -6,6 +6,12 @@ implicit none; private
 
 public :: getInputs
 
+real, parameter:: missing_value = -999.0  !< Missing value
+
+integer :: n_read !< Number of "observations"
+integer :: n_lvl  !< Number of "levels", =1, unless profile observations
+integer :: vrsn   !< Version number of writer/reader
+
 contains
 
 !> This subroutine reads inputs set in: read_bin_qc_obs
@@ -37,5 +43,50 @@ subroutine getInputs(num_inputs, &
     call getarg(2, obsType)
     call getarg(3, obsPlat)
 end subroutine getInputs
+
+
+!integer, dimension(:), allocatable :: &
+!     ob_cyc, ob_ltc, ob_sat, ob_smpl, ob_trck
+
+!real, dimension(:), allocatable :: &
+!     ob_age, ob_lat, ob_lon, ob_qc, ob_ssh, ob_sla
+
+!  character, allocatable :: ob_dtg(:)  *14 
+!  character, allocatable :: ob_rcpt(:) *14 
+! print *, "Reading input file name:" , trim(in_fName)
+! open(10, file=trim(in_fName), status='old', &
+!          access='sequential', form='unformatted')
+! read (10) n_read, n_lvl, vrsn
+
+! if (n_read > 0) then
+!   allocate( ob_age(n_read), ob_cyc(n_read), ob_lat(n_read), ob_lon(n_read), &
+!             ob_qc(n_read), ob_sat(n_read), ob_smpl(n_read), ob_ssh(n_read), &
+!             ob_trck(n_read), ob_ltc(n_read), ob_dtg(n_read), ob_rcpt(n_read), &
+!             ob_sla(n_read))
+
+!   read (10) ob_age
+!   read (10) ob_cyc
+!   read (10) ob_lat
+!   read (10) ob_lon
+!   read (10) ob_qc
+!   read (10) ob_sat
+!   read (10) ob_smpl
+!   read (10) ob_ssh
+!   read (10) ob_trck
+!   read (10) ob_ltc
+!   read (10) ob_dtg
+!   read (10) ob_rcpt
+
+!   if (vrsn > 1) then
+!      read (10) ob_sla
+!   else
+!      ob_sla(:) = missing_value
+!   endif
+
+!   deallocate( ob_age, ob_cyc, ob_lat, ob_lon, &
+!               ob_qc, ob_sat, ob_smpl, ob_ssh, &
+!               ob_trck, ob_ltc, ob_dtg, ob_rcpt, ob_sla)
+! endif
+! close(10)
 
 end module read_bin
--- sorc/rtofs_da.fd/observations/read_bin_qc_obs.F90
+++ sorc/rtofs_da.fd/observations/read_bin_qc_obs.F90
@@ -26,68 +26,45 @@ program read_bin_qc_obs
   character(len=100) :: in_fName       ! Name of the input file
   character(len=100) :: oType, oPlat   ! Observation type and platform
 
-  real, parameter :: missing_value = -999.0      ! Missing value
+  character(len=200) :: output_path    ! Path to output
+  character(len=100) :: out_fName      ! Name of the output file
+
   logical :: exists, input_read_ok
 
-  integer :: n_read, n_lvl, vrsn
-
-  integer, dimension(:), allocatable :: &
-     ob_cyc, ob_ltc, ob_sat, ob_smpl, ob_trck
-
-  real, dimension(:), allocatable :: &
-     ob_age, ob_lat, ob_lon, ob_qc, ob_ssh, ob_sla
-
-  character, allocatable :: ob_dtg(:)  *14
-  character, allocatable :: ob_rcpt(:) *14
-
-! set default
-  input_read_ok = .false.  ! Unless file is found, assume no file is found.
+! Read inputs
+! -----------
+  input_read_ok = .false.  ! Unless input file is found, assume failure.
   
   call getInputs(num_inputs, in_fName, oType, oPlat)
 
   inquire(file=trim(in_fName), exist=exists)
   print *, " "
   if ( exists) then
-    input_read_ok = .true.
+    input_read_ok = .true. ! Input file exists: success!
   else
     print *, "Error reading input file name: " , trim(in_fName)
     stop 'EXIT.'
   end if
 
-  print *, "Reading input file name:" , trim(in_fName)
-  open(10, file=trim(in_fName), status='old', &
-           access='sequential', form='unformatted')
-  read (10) n_read, n_lvl, vrsn
+! Do the work
+! -----------
 
-  if (n_read > 0) then
-    allocate( ob_age(n_read), ob_cyc(n_read), ob_lat(n_read), ob_lon(n_read), &
-              ob_qc(n_read), ob_sat(n_read), ob_smpl(n_read), ob_ssh(n_read), &
-              ob_trck(n_read), ob_ltc(n_read), ob_dtg(n_read), ob_rcpt(n_read), &
-              ob_sla(n_read))
+! Read input file
+  if (oType == "ssh") then
+!   call ssh_reader(in_fName)
+  end if
+! call reader
 
-    read (10) ob_age
-    read (10) ob_cyc
-    read (10) ob_lat
-    read (10) ob_lon
-    read (10) ob_qc
-    read (10) ob_sat
-    read (10) ob_smpl
-    read (10) ob_ssh
-    read (10) ob_trck
-    read (10) ob_ltc
-    read (10) ob_dtg
-    read (10) ob_rcpt
+! Write output file
+  out_fName = trim(in_fName) // ".nc" 
+! call writer
 
-    if (vrsn > 1) then
-       read (10) ob_sla
-    else
-       ob_sla(:) = missing_value
-    endif
 
-    deallocate( ob_age, ob_cyc, ob_lat, ob_lon, &
-                ob_qc, ob_sat, ob_smpl, ob_ssh, &
-                ob_trck, ob_ltc, ob_dtg, ob_rcpt, ob_sla)
-  endif
-  close(10)
+! Finish
+! ------
+  print *, "NetCDF formatted file with contents read from: ", in_fName
+  print *, "Has been written out to: ", out_fName
+  print *, "All done."
+  print *, " "
 
 end program read_bin_qc_obs
