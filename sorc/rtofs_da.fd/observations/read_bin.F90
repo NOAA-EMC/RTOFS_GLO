cb25a9d0805dc6e5d25b534c2908637e9b4f3fd3
sanAkel <santha.akella@gmail.com>
2025-12-07 02:18:26 +0000

    All SSTs are done, including write to nc

--- sorc/rtofs_da.fd/observations/read_bin.F90
+++ sorc/rtofs_da.fd/observations/read_bin.F90
@@ -6,7 +6,9 @@ use netcdf
 
 implicit none
 
-private :: ssh_write_to_netcdf, check
+private :: sst_write_to_netcdf, &
+           ssh_write_to_netcdf, &
+           check
 
 public :: getInputs, &
           sst_converter, &
@@ -103,8 +105,8 @@ subroutine sst_converter(input_file, observation_type, output_path, output_file)
     read (unit) water_mass_class_ind
 
     ! write to netcdf file
-!   call sst_write_to_netcdf(output_path, output_file, n_read, &
-!    bias, dtg, err, flg, lat, lon, qc, sst, sat, water_mass_class_ind)
+    call sst_write_to_netcdf(output_path, output_file, n_read, &
+     bias, dtg, err, flg, lat, lon, qc, sst, sat, water_mass_class_ind)
 
     deallocate( age, bias, dtg, err, flg, lat, lon, &
               qc,  sst, sat, water_mass_class_ind)
@@ -174,10 +176,10 @@ end subroutine ssh_converter
 
 
 !> Writes ssh obsertations to a netCDF file
- subroutine ssh_write_to_netcdf(output_path, output_file, n_read, &
+ subroutine ssh_write_to_netcdf(path, output_file, n_read, &
       sat, cyc, trck, date_str, lat, lon, ssh, sla, qc)
 
-  character(len=*), intent(in) :: output_path, output_file
+  character(len=*), intent(in) :: path, output_file
   integer, intent(in) :: n_read
   integer, dimension(n_read), intent(in) :: sat, cyc, trck
   real, dimension(n_read), intent(in) :: lat, lon, ssh, sla, qc
@@ -188,10 +190,11 @@ end subroutine ssh_converter
   integer :: varid_sat, varid_cyc, varid_trck, varid_date, &
              varid_lat, varid_lon, varid_ssh, varid_sla, varid_qc
   character(len=512) :: file_path
-  character(len=*), parameter :: title = "Quality controlled sea surface height from NCEP RTOFS"
+  character(len=*), parameter :: title = &
+    "Quality controlled sea surface height from NCEP RTOFS"
 
   !  Construct full file path
-  file_path = trim(output_path) // '/' // trim(output_file)
+  file_path = trim(path) // '/' // trim(output_file)
   if (verbose) print *, "Saving file: ", file_path
 
   call check( nf90_create(trim(file_path), NF90_CLOBBER, ncid)) ! Create netCDF file
@@ -232,6 +235,66 @@ end subroutine ssh_converter
   call check( nf90_close(ncid)) ! Close the netCDF file
 end subroutine ssh_write_to_netcdf
 
+
+!> Writes sst obsertations to a netCDF file
+subroutine sst_write_to_netcdf(path, fname, n_read, &
+     bias, date_str, err, flg, lat, lon, qc, sst, sat, wmid)
+
+  character(len=*), intent(in) :: path, fname
+  integer, intent(in) :: n_read
+  integer, dimension(n_read), intent(in) :: flg, sat, wmid
+  real, dimension(n_read), intent(in) :: bias, err, lat, lon, qc, sst
+  character(len=len_sst_date_str), dimension(n_read), intent(in) :: date_str
+
+  ! Local variables
+  integer :: ncid, dimid_n, dimid_len_str
+  integer :: varid_bias, varid_date, varid_err, varid_flg, &
+             varid_lat, varid_lon, varid_qc, varid_sst, varid_sat, varid_wmid
+  character(len=512) :: file_path
+  character(len=*), parameter :: title = &
+    "Quality controlled sea surface temperature from NCEP RTOFS"
+
+  ! Construct full file path
+  file_path = trim(path) // '/' // trim(fname)
+  if (verbose) print *, "Saving file: ", file_path
+
+  call check( nf90_create(trim(file_path), NF90_CLOBBER, ncid)) ! Create netCDF file
+
+  ! Define Dimensions
+  call check( nf90_def_dim(ncid, "n_read", n_read, dimid_n)) 
+  call check( nf90_def_dim(ncid, "string_length", len_sst_date_str, dimid_len_str))
+
+  ! Define global attribute
+  call check( nf90_put_att(ncid, NF90_GLOBAL, "title", trim(title)))
+
+  ! Define Variables
+  call check( nf90_def_var(ncid, "bias", NF90_FLOAT, dimid_n, varid_bias))
+  call check( nf90_def_var(ncid, "date", NF90_CHAR,  (/dimid_len_str, dimid_n/), varid_date))
+  call check( nf90_def_var(ncid, "err",  NF90_FLOAT, dimid_n, varid_err))
+  call check( nf90_def_var(ncid, "flag", NF90_INT,   dimid_n, varid_flg))
+  call check( nf90_def_var(ncid, "lat",  NF90_FLOAT, dimid_n, varid_lat))
+  call check( nf90_def_var(ncid, "lon",  NF90_FLOAT, dimid_n, varid_lon))
+  call check( nf90_def_var(ncid, "qc",   NF90_FLOAT, dimid_n, varid_qc))
+  call check( nf90_def_var(ncid, "sst",  NF90_FLOAT, dimid_n, varid_sst))
+  call check( nf90_def_var(ncid, "sat_id",        NF90_INT,   dimid_n, varid_sat))  ! Satellite ID
+  call check( nf90_def_var(ncid, "water_mass_id", NF90_INT,   dimid_n, varid_wmid)) !Water Mass Index
+
+  call check( nf90_enddef(ncid)) ! End define mode
+
+  ! Write data to variables
+  call check( nf90_put_var(ncid, varid_bias, bias))
+  call check( nf90_put_var(ncid, varid_date, date_str))
+  call check( nf90_put_var(ncid, varid_err,  err))
+  call check( nf90_put_var(ncid, varid_flg,  flg))
+  call check( nf90_put_var(ncid, varid_lat,  lat))
+  call check( nf90_put_var(ncid, varid_lon,  lon))
+  call check( nf90_put_var(ncid, varid_qc,   qc))
+  call check( nf90_put_var(ncid, varid_sst,  sst))
+  call check( nf90_put_var(ncid, varid_sat,  sat))
+  call check( nf90_put_var(ncid, varid_wmid, wmid))
+
+  call check( nf90_close(ncid)) ! Close the netCDF file
+end subroutine sst_write_to_netcdf
 
 !> From https://home.chpc.utah.edu/~thorne/computing/Examples_netCDF.pdf
 subroutine check(istatus)
