ef6ebf4976b8d4416a2c25fea16c638fff57a8cf
sanAkel <santha.akella@gmail.com>
2026-01-02 18:40:23 +0000

    Done. Next: prestaging.sh

--- scripts/exrtofs_glo_analysis_pre.sh
+++ scripts/exrtofs_glo_analysis_pre.sh
@@ -1,21 +1,4 @@
 #!/bin/sh
-###############################################################################
-####  UNIX Script Documentation Block                                         #
-#                                                                             #
-# Script name:         exrtofs_glo_analysis_pre.sh                            #
-# Script description:                                                         #
-#                                                                             #
-# Author:        Ilya Rivin      Org: NP23         Date: 2010-07-30           #
-#                                                                             #
-# Abstract: This script generates input fields for                            #
-#           for the RTOFS_GLO Ocean model analysis.                           #
-#                                                                             #
-# Sub-scripts called:                                                         #
-#                                                                             #
-# Script history log:                                                         #
-# 2010-07-30  Ilya Rivin                                                      #
-#                                                                             #
-###############################################################################
 set -xa
 
 export PS4='$SECONDS + '
@@ -28,22 +11,23 @@ postmsg "$msg"
 # --------------------------------------------------------------------------- #
 # 0. date and time stuff
 
+# Get 48 hours for NCODA (first record of forcings is not to be used)
+  export forcingHRS=48
+
   export fcstdays=${fcstdays:-2}
   export enddate=${analysis_end:-${PDY}${mycyc}}
-# startdate for V2 is 24 hours, but get 48 hours for NCODA purposes (and bug on first record of forcings)
-  startice=$($NDATE -$(expr $fcstdays \* 24) ${enddate})
-  export iday=$($USHrtofs/rtofs_date_normal2hycom.sh $startice)
-  export startdate=$($NDATE -$(expr $fcstdays \* 48) ${enddate})
-#-- del export inputgrid=${inputgrid:-navy_0.08}
+  export startdate=$($NDATE -$(expr $fcstdays \* ${forcingHRS}) ${enddate})
 
 # --------------------------------------------------------------------------- #
 # 1  Set up the start time and end time for the analysis
-  sday=$($USHrtofs/rtofs_date_normal2hycom.sh $startdate)
-  eday=$($USHrtofs/rtofs_date_normal2hycom.sh $enddate)
+
+  sday=$($USHrtofs/days_past_refDate.sh $startdate)
+  eday=$($USHrtofs/days_past_refDate.sh $enddate)
   echo "  $sday $eday false false  " > limits
 
 # --------------------------------------------------------------------------- #
 # 2. Do staging
+
   $USHrtofs/${RUN}_prestaging.sh 
 
 #################################################
