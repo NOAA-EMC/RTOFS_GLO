628be879f72c72f85c1c23253e0bf3080663c3ee
sanAkel <santha.akella@gmail.com>
2026-01-02 19:39:42 +0000

    Next rtofs_atmforcing.sh

--- ush/rtofs_prestaging.sh
+++ ush/rtofs_prestaging.sh
@@ -1,13 +1,4 @@
-#!/bin/ksh
-#########################################################################
-# Usage: rtofs_prestaging.sh                                            #
-#                                                                       #
-# Description: Staging input files for analysis or forecast             #
-#              pre-processing                                           #
-#                                                                       #
-# History:                                                              #
-#    07-30-2010  Ilya Rivin                                             #
-#########################################################################
+#!/bin/sh
 set -xa
 
 echo "*** Started script $0 on hostname "$(hostname)' at time '$(date)
@@ -16,158 +7,57 @@ export PS4='$SECONDS + '
 cd $DATA
 
 # --------------------------------------------------------------------------- #
-# 1  Set parameters depending on run mode
-if [ ${RUN_MODE} = "analysis" ]
-then
-  runmode='anal'
-  runstep=${runmode}
-  runname='ANALYSIS_PRE'
-  # Set intvl=3 for retrospectives when only 3 hour forcing is available.
-  # Remember to change 3 lines in parm/rtofs_glo.navy_0.08.anal.ice_in.
-  # Change atm_data_type, sss_data_type, sst_data_type from 'cfsr' to 'nogaps'.
-  intvl=1 #atmospheric forcing frequency
+# 1.  Set parameters depending on run mode
+
+if [[ "${RUN_MODE}" == "analysis" ]]; then
+    runmode="anal"
+    runstep="${runmode}"
+    runname="ANALYSIS_PRE"
+  # Set intvl=3 for retrospectives when only 3 hr forcing is available.
+    intvl=1  # Atmospheric forcing frequency: 1hr
 else
-  runmode='fcst'
-  runstep=${runmode}${stepnum}
-  runname='FORECAST_PRE'
-  intvl=3 #atmospheric forcing frequency
-fi 
+    runmode="fcst"
+    runstep="${runmode}${stepnum}"
+    runname="FORECAST_PRE"
+    intvl=3  # Atmospheric forcing frequency: 3hr
+fi
 
 # --------------------------------------------------------------------------- #
-# 1  Set up the start time and end time for the analysis or forecast
-  sday=$($USHrtofs/rtofs_date_normal2hycom.sh $startdate)
-  eday=$($USHrtofs/rtofs_date_normal2hycom.sh $enddate)
+# 2.  Set up the start and end times for the analysis or forecast
+
+  sday=$($USHrtofs/days_past_refDate.sh $startdate)
+  eday=$($USHrtofs/days_past_refDate.sh $enddate)
   echo "  $sday $eday false false  " > limits
 
-# 2. Copy the necessary input files for the model forecast
-
-# 2.a Get topography (depth, grid, mask, cice) and offluxxg
-# -- del for fil in depth grid
-# -- del  do
-# -- del    for type in a b
-# -- del    do
-# -- del      if [ -s $FIXrtofs/${RUN}_${modID}.${inputgrid}.regional.${fil}.${type} ]
-# -- del      then
-# -- del        ln -s -f $FIXrtofs/${RUN}_${modID}.${inputgrid}.regional.${fil}.${type} regional.${fil}.${type}
-# -- del        echo "regional.${fil}.${type} copied" 
-# -- del      else
-# -- del        $USHrtofs/${RUN}_abort.sh "FATAL ERROR: $job Missing Topography files" \
-# -- del          "NO FILE for regional.${fil}.${type}" 2
-# -- del      fi
-# -- del    done
-# -- del  done
-#
-# Relax files for SSH are needed for restart2archive only
-# -- del  for type in a b
-# -- del  do
-# -- del      if [ -s $FIXrtofs/${RUN}_${modID}.${inputgrid}.relax_ssh.${type} ]
-# -- del      then
-# -- del        ln -s -f $FIXrtofs/${RUN}_${modID}.${inputgrid}.relax_ssh.${type} relax.ssh.${type}
-# -- del        echo "relax.ssh.${type} copied" 
-# -- del      else
-# -- del        $USHrtofs/${RUN}_abort.sh "FATAL ERROR: $job Missing SSH Relaxation files" \
-# -- del          "NO FILE for relax.ssh.${type}" 2
-# -- del      fi
-# -- del   done
-
-# -- del if [ -s $FIXrtofs/${RUN}_${modID}.${inputgrid}.regional.cice.r ]
-# -- del then
-# -- del   ln -s -f $FIXrtofs/${RUN}_${modID}.${inputgrid}.regional.cice.r regional.cice.r
-# -- del   echo "regional.cice.r copied" 
-# -- del else
-# -- del   $USHrtofs/${RUN}_abort.sh "FATAL ERROR: $job Missing Topography files" \
-# -- del     "NO FILE for regional.cice.r" 2
-# -- del fi
-# -- del if [ -s $FIXrtofs/${RUN}_${modID}.${inputgrid}.forcing.offlux.a ] \
-# -- del   && [ -s $FIXrtofs/${RUN}_${modID}.${inputgrid}.forcing.offlux.b ] 
-# -- del then
-# -- del   ln -s -f $FIXrtofs/${RUN}_${modID}.${inputgrid}.forcing.offlux.a  forcing.offlux.a 
-# -- del   ln -s -f $FIXrtofs/${RUN}_${modID}.${inputgrid}.forcing.offlux.b  forcing.offlux.b 
-# -- del   echo "forcing.offlux.[ab] copied" 
-# -- del else
-# -- del   $USHrtofs/${RUN}_abort.sh "FATAL ERROR: $job Missing Topography files" \
-# -- del     "NO FILE for forcing.offlux.[ab]" 2
-# -- del fi
-
-# 2.h copy interpolator data
-# -- del   cp $PARMrtofs/${RUN}_${modID}.${inputgrid}.${runmode}.intp_pars.dat intp_pars.dat
-
-# -- del   if [[ -s intp_pars.dat ]]
-# -- del   then
-# -- del       echo "interpolator data copied"
-# -- del   else
-# -- del       $USHrtofs/${RUN}_abort.sh "FATAL ERROR: $job Missing Interpolator Data" \
-# -- del         "NO FILE for intp_pars.dat" 2
-# -- del   fi
-
+# --------------------------------------------------------------------------- #
 # 3. Create Forcing files
 
-# 3.a call atmforcing to get the forcing files 
+# 3.a Call atmforcing to get the forcing files 
 
   $USHrtofs/${RUN}_atmforcing.sh $startdate $enddate $intvl
 
-  echo '-- INTENTIONAL EXIT -- '
-  exit 998
-
-# 3.b correct air temperature over ice.
+# 3.b Correct air temperature over ice.
    
   # Ilya dbgz: the correction is done now in intp.f. 
   # Anyhow, atmforcing_correct.f90 should be debugged!!!
-  $USHrtofs/${RUN}_atmforcing_correct.sh
+# $USHrtofs/${RUN}_atmforcing_correct.sh
 
-  # Prepare sea ice forcing
-  $USHrtofs/${RUN}_iceforcing.sh
+# --------------------------------------------------------------------------- #
+# 4. Copy output in archive -- ?? Dan: Do we need the following ??
 
-# 4. Copy output in archive
+  # temporarily retaining to remind myself of forcing file template
+  #cp -p forcing.${fil}.${type} ${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.forcing.${fil}.${type} 
 
-  cp -p $DATA/listflx.dat ${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.listflx.dat
-  for fil in airtmp glbrad lwdflx precip presur radflx shwflx surtmp vapmix wndewd wndnwd wndspd 
-  do
-    for type in a b
-    do
-      if [ -s forcing.${fil}.${type} ]
-      then
-        cp -p forcing.${fil}.${type} ${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.forcing.${fil}.${type} 
-        echo "forcing.${fil}.${type} copied" 
-          if [ $SENDDBN = YES ]
-          then
-            if [[ ${type} = a ]]; then
-              $DBNROOT/bin/dbn_alert MODEL RTOFS_GLO_FORCINGA $job \
-              ${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.forcing.${fil}.${type}
-            else
-              msg="File${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.forcing.${fil}.${type} not posted to db_net."
-              postmsg "$msg"
-            fi
-          fi
-       else
-        $USHrtofs/${RUN}_abort.sh "FATAL ERROR: $job Missing Fix Forcing File" \
-          "NO FILE for forcing.${fil}.${type}" 2
-      fi
-    done
-  done
-  for fil in airtmp glbrad lwdflx vapmix wndewd wndnwd
-  do
-    for type in r B
-    do
-      if [ -s cice.${fil}.${type} ]
-      then
-        cp -p cice.${fil}.${type} ${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.cice.${fil}.${type} 
-        echo "forcing.${fil}.${type} copied" 
-          if [ $SENDDBN = YES ]
-          then
-            if [[ ${type} = r ]]; then
-              $DBNROOT/bin/dbn_alert MODEL RTOFS_GLO_CICER $job \
-              ${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.cice.${fil}.${type}
-            else
-              msg="File ${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.cice.${fil}.${type} not posted to db_net."
-              postmsg "$msg"
-            fi
-          fi
-      else
-        $USHrtofs/${RUN}_abort.sh "FATAL ERROR: $job Missing cice Forcing File" \
-          "NO FILE for cice.${fil}.${type}" 2
-      fi
-    done
-  done
+  # Check
+  #if [[ ${type} = a ]]; then
+  #  $DBNROOT/bin/dbn_alert MODEL RTOFS_GLO_FORCINGA $job \
+  #  ${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.forcing.${fil}.${type}
+  #else
+  #  msg="File${COMOUT}/${RUN}_${modID}.${runstep}.t${mycyc}z.forcing.${fil}.${type} not posted to db_net."
+  #  postmsg "$msg"
+  #fi
+
+  #$USHrtofs/${RUN}_abort.sh "FATAL ERROR: $job Missing Fix Forcing File" \
+  #"NO FILE for forcing.${fil}.${type}" 2
 
 echo "*** Finished script $0 on hostname "$(hostname)' at time '$(date)
