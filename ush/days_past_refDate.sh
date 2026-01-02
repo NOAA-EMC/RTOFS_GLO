89d779caed227c3a0012599ac510107fd4da9299
sanAkel <santha.akella@gmail.com>
2026-01-02 18:39:29 +0000

    1. Rename to (better) reflect purpose, 2. Eliminate hycom naming

---
+++ ush/days_past_refDate.sh
@@ -0,0 +1,19 @@
+#!/bin/ksh
+set -x
+############################################################
+# This script converts the date to the day since 1901010100
+#
+# Example usage: days_path_refDate.sh 2001010200
+#
+#############################################################
+# Set the basetime corresponding to the date 1901010100 
+btime=1900123100 
+
+# Set the current date
+cdate=$(echo $1 | cut -c1-10)          
+
+chour=$($NHOUR $cdate $btime)
+cday=$(expr $chour / 24)
+typeset -Z2 residual=$(expr $chour % 24 \* 100 / 24) 
+daymodel=${cday}.${residual}
+echo $daymodel
