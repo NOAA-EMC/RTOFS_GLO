dc1559efe1ab8104bcdd48284743cf77cc15362f
sanAkel <santha.akella@gmail.com>
2026-05-12 16:23:50 +0000

    fix NCODA clone and fix file path

--- sorc/build_ncoda_and_rtofs_utils.sh
+++ sorc/build_ncoda_and_rtofs_utils.sh
@@ -11,20 +11,44 @@ if [[ $# -ne 1 ]]; then
 fi
 
 MACHINE=$1
+
+# Exit if machine is not exactly "wcoss2"
+if [[ "$MACHINE" != "wcoss2" ]]; then
+    echo "ERROR: Unsupported machine '$MACHINE', currently supported on 'wcoss2' only."
+    exit 1
+fi
+
 echo ">>> Target machine set to: $MACHINE"
 # Exporting MACHINE in case the underlying scripts rely on it as an environment variable
 export MACHINE
 
 set -eux
 
-# 1. Build Libraries
+# 1. Build Libraries/Utilities
 echo ">>> Building libraries..."
 cd ../libs
 ./build_libs.sh
-
-# 2. Build and Install RTOFS
-echo ">>> Building and installing RTOFS model..."
 cd ../sorc
+
+# 2. Build and Install NCODA
+echo ">>> Building and installing RTOFS model..."
+
+# 2.1 Clone NCODA
+echo ">>> Cloning NCODA repository..."
+if [[ ! -d "rtofs_ncoda.fd" ]]; then
+   git clone git@github.com:NOAA-EMC/NCODA.git rtofs_ncoda.fd
+else
+   echo ">>> rtofs_ncoda.fd already exists, skipping clone."
+fi
+
+# 2.2 Setup fix directory symlink
+# Moving up to the directory above to create the link
+cd ..
+echo ">>> Linking fix directory..."
+# The -n flag ensures it doesn't nest the link if run multiple times
+ln -sfn /lfs/h2/emc/couple/noscrub/dan.iredell/RTOFSFIX/20260219/fix/ fix
+
+cd sorc
 ./build_rtofs.sh || { echo "ERROR: build_rtofs.sh failed."; exit 1; }
 ./build_rtofs.sh install || { echo "ERROR: build_rtofs.sh install failed."; exit 1; }
 
