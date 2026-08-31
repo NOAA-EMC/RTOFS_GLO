1c9fe42454b952d67a5e6d769d5e13b84f3131f1
sanAkel <santha.akella@gmail.com>
2026-08-31 22:45:01 +0000

    clean setting of CICE IC

--- scripts/pre/get_ic_paths.sh
+++ scripts/pre/get_ic_paths.sh
@@ -8,15 +8,21 @@ fi
 machine=$(echo "${MACHINE_ID}" | cut -d. -f1)
 
 case "${machine}" in
-    "wcoss2") INPUTDATA_ROOT="/lfs/h2/emc/nems/noscrub/emc.nems/RT/NEMSfv3gfs/input-data-20260617" ;;
-    "orion" | "hercules") INPUTDATA_ROOT="/work/noaa/nems/role-nems/RT/NEMSfv3gfs/input-data-20210825" ;;
+    "wcoss2")
+        INPUTDATA_ROOT="/lfs/h2/emc/nems/noscrub/emc.nems/RT/NEMSfv3gfs/input-data-20260617"
+        MOM6_IC_DIR="${INPUTDATA_ROOT}/MOM6_IC/${OCNRES}/2011100100"
+        CICE_IC_FILE="${INPUTDATA_ROOT}/CICE_IC/${OCNRES}/cice_model_${ICERES}.cpc.res_2011100100.nc"
+        ;;
+    "orion" | "hercules")
+        INPUTDATA_ROOT="/work/noaa/nems/role-nems/RT/NEMSfv3gfs/input-data-20210825"
+        MOM6_IC_DIR="${INPUTDATA_ROOT}/MOM6_IC/${OCNRES}/2011100100"
+        CICE_IC_FILE="${INPUTDATA_ROOT}/CICE_IC/${OCNRES}/cice_model_${ICERES}.cpc.res_2011100100.nc"
+        ;;
     *)
-        echo "FATAL: Unknown target machine ${machine}, couldn't set INPUTDATA_ROOT"
+        echo "FATAL: Unknown target machine ${machine}"
+        echo "couldn't set INPUTDATA_ROOT, MOM6_IC_DIR, and CICE_IC_FILE"
         return 1 2>/dev/null || exit 1
         ;;
 esac
 
-export INPUTDATA_ROOT
-
-export MOM6_IC_DIR="${INPUTDATA_ROOT}/MOM6_IC/${OCNRES}/2011100100"
-export CICE_IC_DIR="${INPUTDATA_ROOT}/CICE_IC/${OCNRES}"
+export INPUTDATA_ROOT MOM6_IC_DIR CICE_IC_FILE
