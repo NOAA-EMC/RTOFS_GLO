b71717deebf0acceeb15351d32648c3a03ecc75f
sanAkel <santha.akella@gmail.com>
2026-09-10 17:20:05 +0000

    no need for MAP_DIR, those files already exist!

--- scripts/pre/get_forcing_paths_008.sh
+++ scripts/pre/get_forcing_paths_008.sh
@@ -10,9 +10,6 @@ machine=$(echo "${MACHINE_ID}" | cut -d. -f1)
 case "${machine}" in
     "ursa")
         FORCING_FILE="/scratch5/NCEPDEV/rstprod/Santha.Akella/data/forcing/zg/gfs.2025121400-2025123118_positive.nc"
-
-        # Path to the map.* files needed for ESMF weights mapping.
-        MAP_DIR="/scratch5/NCEPDEV/rstprod/Santha.Akella/data/map_files/mx008"
         ;;
     "wcoss2" | "acorn" | "orion")
         echo "FATAL: 0.08 forcing paths not yet defined for ${machine}"
@@ -24,4 +21,4 @@ case "${machine}" in
         ;;
 esac
 
-export FORCING_FILE MAP_DIR
+export FORCING_FILE
